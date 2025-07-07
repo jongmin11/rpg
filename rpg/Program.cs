3f700d9e2db2a3e2e4b3c2185c6381e237a7cbbe
jongmin11 <gkswhdals01@gmail.com>
2025-07-07 12:27:12 +0900

    중간점검1

--- rpg/Program.cs
+++ rpg/Program.cs
@@ -1,3 +1,27 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine(3+2+3);  
+﻿namespace rtan
+{
+    
+    class rpg
+    {
+        static void Main(String[] args) 
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다 \n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Console.WriteLine("\n");
+            Console.WriteLine("1.\t상태보기");
+            Console.WriteLine("2.\t인벤토리");
+            Console.WriteLine("3.\t상점");
+
+            if ()
+
+
+
+        }
+    }
+
+
+
+}
+
+
